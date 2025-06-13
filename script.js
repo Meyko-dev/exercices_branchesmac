5437762ea4880b16ad22764ddc4153e46589563e
Meyko_Dev <raphaelle.noel@gmail.com>
2025-06-13 17:23:50 +0200

    BonjourJavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+/*let message = "Bonjour" 
+let firstName = "Beyonce !"
+console.log(message + " " + firstName);*/
+
+let message = "Bonjour" 
+let firstName = "Beyonce !"
+function sayHello(message, firstName){
+} console.log(message + " " + firstName)
